3d62ef0ba67eb759803372d4b220df78e146de0c
Andika Sujanadi <andikasujanadi@gmail.com>
2025-02-23 03:09:09 +0800

    windows setup

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
@@ -17,16 +18,23 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "hyperion",
-		Width:  1024,
-		Height: 768,
+		Title:     "hyperion",
+		Width:     1024,
+		Height:    768,
+		MinWidth:  400,
+		MinHeight: 400,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		BackgroundColour: &options.RGBA{R: 0, G: 0, B: 0, A: 0},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
+		},
+		Windows: &windows.Options{
+			WebviewIsTransparent: true,
+			WindowIsTranslucent:  true,
+			BackdropType:         windows.Mica,
 		},
 	})
 
