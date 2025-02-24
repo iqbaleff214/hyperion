aea63f17f89a629a0df19fabd51b629481369953
Andika Sujanadi <andikasujanadi@gmail.com>
2025-02-24 20:10:29 +0800

    remove transparency in mac fullscreen

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -15,6 +15,13 @@
   let obfuscatedContent = {};
   let showContent = {};
   let isMac = navigator.userAgent.includes("Mac");
+  let isFullscreen = false;
+
+  function checkFullscreen() {
+    isFullscreen = window.outerHeight === screen.height;
+  }
+
+  window.addEventListener("resize", checkFullscreen);
 
   async function openFiles() {
     try {
@@ -108,7 +115,7 @@
   }
 </script>
 
-<div class="flex flex-col h-full {isMac?'bg-white/50 dark:bg-neutral-900/50':''}">
+<div class="flex flex-col h-full {isMac?isFullscreen?'bg-white dark:bg-neutral-900':'bg-white/50 dark:bg-neutral-900/50':''}">
   <div style="--wails-draggable:drag; padding-top: {isMac ? '32px' : '8px'}" class="flex flex-wrap gap-1 px-4 pb-2">
     <button
       class="bg-white dark:bg-neutral-900 text-sm border border-black/15 dark:border-white/15 text-black dark:text-white disabled:text-black/50 dark:disabled:text-white/50 px-3 py-1 rounded enabled:cursor-pointer flex gap-1 items-center"
